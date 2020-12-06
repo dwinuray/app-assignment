98e27038849a58fdb349eaf3ba86357d4d8be008
Dwi Nur Cahyo <47982352+dwinuray@users.noreply.github.com>
2020-12-06 14:26:50 +0700

    Update issue EditText type date, after that i added some plugin datepicker on add assignment menu.

--- app/src/main/java/com/dwinuray/app_assignment/ui/FragmentAdd.java
+++ app/src/main/java/com/dwinuray/app_assignment/ui/FragmentAdd.java
@@ -9,8 +9,11 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -22,11 +25,24 @@ import android.widget.Toast;
 import com.dwinuray.app_assignment.R;
 import com.dwinuray.app_assignment.databases.Database;
 import com.dwinuray.app_assignment.models.Assignments;
+import com.kunzisoft.switchdatetime.SwitchDateTimeDialogFragment;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+import java.util.TimeZone;
 
 public class FragmentAdd extends Fragment {
 
     private Database db;
+
+    private static final String TAG = "Sample";
+    private static final String TAG_DATETIME_FRAGMENT = "TAG_DATETIME_FRAGMENT";
+    private static final String STATE_TEXTVIEW = "STATE_TEXTVIEW";
+    private SwitchDateTimeDialogFragment dateTimeFragment;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -53,6 +69,68 @@ public class FragmentAdd extends Fragment {
         description = view.findViewById(R.id.txtDescription);
         date = view.findViewById(R.id.txtDate);
         type = view.findViewById(R.id.radioGroup);
+
+
+
+        // Construct SwitchDateTimePicker
+        dateTimeFragment = (SwitchDateTimeDialogFragment) getParentFragmentManager().findFragmentByTag(TAG_DATETIME_FRAGMENT);
+        if(dateTimeFragment == null) {
+            dateTimeFragment = SwitchDateTimeDialogFragment.newInstance(
+                    getString(R.string.label_datetime_dialog),
+                    getString(android.R.string.ok),
+                    getString(android.R.string.cancel)
+            );
+        }
+
+        // Optionally define a timezone
+        dateTimeFragment.setTimeZone(TimeZone.getDefault());
+
+        // Init format
+        final SimpleDateFormat myDateFormat = new SimpleDateFormat("d MMM yyyy HH:mm", Locale.getDefault());
+        // Assign unmodifiable values
+        dateTimeFragment.set24HoursMode(false);
+        dateTimeFragment.setHighlightAMPMSelection(false);
+        dateTimeFragment.setMinimumDateTime(new GregorianCalendar(2015, Calendar.JANUARY, 1).getTime());
+        dateTimeFragment.setMaximumDateTime(new GregorianCalendar(2025, Calendar.DECEMBER, 31).getTime());
+
+        // Define new day and month format
+        try {
+            dateTimeFragment.setSimpleDateMonthAndDayFormat(new SimpleDateFormat("MMMM dd", Locale.getDefault()));
+        } catch (SwitchDateTimeDialogFragment.SimpleDateMonthAndDayFormatException e) {
+            Log.e(TAG, e.getMessage());
+        }
+
+        // Set listener for date
+        // Or use dateTimeFragment.setOnButtonClickListener(new SwitchDateTimeDialogFragment.OnButtonClickListener() {
+        dateTimeFragment.setOnButtonClickListener(new SwitchDateTimeDialogFragment.OnButtonWithNeutralClickListener() {
+            @Override
+            public void onPositiveButtonClick(Date paramDate) {
+                date.setText(myDateFormat.format(paramDate));
+            }
+
+            @Override
+            public void onNegativeButtonClick(Date paramDate) {
+                // Do nothing
+            }
+
+            @Override
+            public void onNeutralButtonClick(Date paramDate) {
+                // Optional if neutral button does'nt exists
+                date.setText("");
+            }
+        });
+
+
+        date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Re-init each time
+                dateTimeFragment.startAtCalendarView();
+                dateTimeFragment.setDefaultDateTime(new GregorianCalendar(2020, Calendar.DECEMBER, 1, 15, 20).getTime());
+                dateTimeFragment.show(getChildFragmentManager(), TAG_DATETIME_FRAGMENT);
+            }
+        });
+
 
         Button btnSave =  view.findViewById(R.id.btnSave);
         btnSave.setOnClickListener(new View.OnClickListener() {
@@ -116,4 +194,6 @@ public class FragmentAdd extends Fragment {
             }
         });
     }
+
+
 }
